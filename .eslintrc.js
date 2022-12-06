669553d01f1aa1d3e3589ea861a181f2a976ce3c
carloscruzyearup <103535773+carloscruzyearup@users.noreply.github.com>
2022-12-06 09:32:20 -0800

    update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: 'standard',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {
-	  'semi': [2, 'always'],
-    'indent': ['error', 4],
-  }
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: 'standard',
+    overrides: [
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    rules: {
+        'semi': [2, 'always'],
+        'indent': ['error', 4]
+    }
 }
